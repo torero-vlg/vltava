b7ecef987f5c775d941fc99a8db7138e1ad49305
Artem <artem.v.rusanov@gmail.com>
2018-06-07 20:12:53 +0300

    vlt-3 гостевая книга фронт-енд

---
+++ T034/Scripts/app/guestbook/addoredit.js
@@ -0,0 +1,29 @@
+﻿define(['layout'], function (layout) {
+
+    function addOrEdit() {
+        $.ajax({
+            url: '/GuestBook/AddOrEdit',
+            method: 'POST',
+            data: {
+                Author: $('#Author').val(),
+                Id: $('#Id').val(),
+                Contact: $('#Contact').val(),
+                Date: $('#Date').val(),
+                Message: $('#Message').val()
+            }
+        })
+        .complete(function (data) {
+            layout.showResult(data);
+        });
+    }
+
+    return {
+        Initialize: function () {
+         
+            $("#btnAddOrEdit").click(function () {
+                addOrEdit();
+            });
+
+        }
+    }
+});
--- T034/Scripts/app/layout.js
+++ T034/Scripts/app/layout.js
@@ -1,11 +1,35 @@
 ﻿define([], function () {
 
-
-
     return {
         Initialize: function () {
 
+            $('#successContainer').hide();
+            $('#errorContainer').hide();
 
+            $('.alert').click(function () {
+                $(this).hide();
+            });
+
+        },
+
+        showResult: function (data) {
+
+            var response = JSON.parse(data.responseText);
+
+            if (response.Status != 0) {
+                $('#errorContainer').show();
+                $('#successContainer').hide();
+                $('#errorContainer').html(response.Message);
+            }
+            else {
+                $('#errorContainer').hide();
+                $('#successContainer').show();
+                $('#successContainer').html(response.Message);
+            }
+
+            setTimeout(function () {
+                $('.alert').hide(1000);
+            }, 5000);
         }
     }
 });
