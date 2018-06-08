befb04728d863ffe57c2030a7806ac6487ed89d9
Артём <artem.v.rusanov@gmail.com>
2018-06-08 07:49:51 +0300

    vlt-3 интеграционные тесты

---
+++ T034.IntegrationTests/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Db.Services;
+using Db.Services.Common;
+
+namespace T034.IntegrationTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emailConfig = new EmailConfig
+            {
+                Server = "robots.1gb.ru",
+                From = "test@t034.ru",
+                To = "test@t034.ru",
+                UserName = "u474469",
+                Password = "Qwerty1"
+            };
+            var emailService = new EmailService(emailConfig);
+
+            emailService.SendEmail("123", "QWERTYY");
+        }
+    }
+}
