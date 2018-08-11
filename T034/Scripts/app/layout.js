d30bc178deafcdd4a8481852b9f8880b3bdeff2f
s.s.blaginina <s.s.blaginina@gmail.com>
2018-08-11 08:44:28 +0300

    vlt-3 дизайн

--- T034/Scripts/app/layout.js
+++ T034/Scripts/app/layout.js
@@ -11,14 +11,18 @@
             });
 
             //отображение меню при прокрутке
-            $(document).on("scroll", function () {
+            $(document).on("scroll", function (eventArgs) {
+                console.log($(document).scrollTop());
+             //   $('.navbar-fixed-top').css('top', 200 - $(document).scrollTop());
+
                 if ($(document).scrollTop() > 199) {
                     $('#banner').hide();
-                    $("#menu").addClass("smallmenu");
+                  //  $("#menu").addClass("smallmenu");
+                    $('.navbar-fixed-top').css('top', 0);
                 }
                 else {
                     $('#banner').show();
-                    $("#menu").removeClass("smallmenu");
+                    $('.navbar-fixed-top').css('top', 200 - $(document).scrollTop());
                 }
 
                 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
